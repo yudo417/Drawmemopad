b83b0a866a4452154a4c55d156fc70654121ad8a
yudo417 <hayashi.kazuki.hk@gmail.com>
2024-12-21 18:49:41 +0900

    Initial Commit

---
+++ Drawmemopad/DrawmemopadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawmemopadApp.swift
+//  Drawmemopad
+//
+//  Created by 林　一貴 on 2024/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawmemopadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrawmemopadTests/DrawmemopadTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrawmemopadTests.swift
+//  DrawmemopadTests
+//
+//  Created by 林　一貴 on 2024/12/21.
+//
+
+import Testing
+@testable import Drawmemopad
+
+struct DrawmemopadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
